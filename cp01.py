b10ca03eadcd77883f8abdf3c01269695b129c0b
JHB11Hinson <lemonoutput@foxmail.com>
2025-03-26 15:26:40 +0800

    开始数据预处理

--- cp01.py
+++ cp01.py
@@ -3,9 +3,15 @@ import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
 
-n_points = 100
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+LEARNING_RATE = 0.1
+n_epochs = 1000
+n_points = 500
 data = torch.rand(n_points, 2) * 2 - 1
 labels = (data.norm(dim=1) > 0.7).float().unsqueeze(1)
+data = data.to(device)
+labels = labels.to(device)
 
 # 创建模型类
 class CircleClassifier(nn.Module):
@@ -20,8 +26,38 @@ class CircleClassifier(nn.Module):
         return x
 
 
+# 创建模型实例
+model = CircleClassifier()
+model = model.to(device)
 
-plt.scatter(data[:, 0],data[:, 1],c=(labels.squeeze() > 0.5), cmap="coolwarm")
+loss_fn = nn.BCELoss() # 二分类交叉熵损失函数
+
+optimizer = optim.AdamW(model.parameters(), lr=LEARNING_RATE)# 随机梯度下降优化器
+
+for epoch in range(n_epochs):
+    optimizer.zero_grad() # 梯度清零
+    predictions = model(data) # 前向传播
+    loss = loss_fn(predictions, labels)
+    loss.backward() # 反向传播
+    optimizer.step() # 更新参数
+
+
+    if epoch % 10 == 0:
+        print(f"Epoch {epoch}, Loss: {loss.item():.4f}")
+
+if device == "cuda":
+    print("Using GPU")
+else:
+    print("Using CPU")
+
+predictions = model(data)
+predictions = predictions.cpu()
+data = data.cpu()
+
+
+
+
+plt.scatter(data[:, 0],data[:, 1],c=(predictions.squeeze() > 0.5), cmap="coolwarm")
 plt.title("Data")
 plt.xlabel("x")
 plt.ylabel("y")
---
+++ cp02.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn import functional as F
+import matplotlib.pyplot as plt
+
+
+# ------数据预处理------
+
