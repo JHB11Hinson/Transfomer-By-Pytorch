99893e6b131e00c77f0d65bda552940a762281d5
JHB11Hinson <lemonoutput@foxmail.com>
2025-04-01 14:09:06 +0800

    Update cp02.py

--- cp02.py
+++ cp02.py
@@ -4,6 +4,10 @@ import torch.optim as optim
 from torch.nn import functional as F
 import matplotlib.pyplot as plt
 
+block_size = 5 # 字符串长度
+batch_size = 8 #同时平行处理的序列个数
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 torch.manual_seed(1337)
 file_name = "hongloumeng_short.txt"
 
@@ -34,5 +38,13 @@ print(f"文件{file_name}读取完成")
 
 def get_batch(split) :
     data = train_data if split == "train" else val_data
-print(data)
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i + block_size] for i in ix])
+    y = torch.stack([data[i + 1:i + block_size + 1] for i in ix]) # 目标是预测下一个字符
+    x. y = x.to(device), y.to(device)
+    return x, y
+
+# ix = get_batch("train")
 # print(chars)
+
+print(get_batch("train"))
